25a0e487267e59f7e54a25a953777de8fd38255f
coderconvoy <thebeaky@psmail.net>
2018-01-04 19:54:01 +0000

    Connections and steps are basically working

--- board.js
+++ board.js
@@ -6,11 +6,14 @@ basehex = {
     hexType:"Water"
 };
 
+basehex.inBoard = function(){
+    return (this.hexType !== "Water") && (this.hexType !== "Land")
+}
+
 basehex.string = function(){
     res = this.hexType + " ";
-    switch(this.hexType){
-        case "Water" , "Land": return res;
-    }
+    if (! this.inBoard()) return res;
+
     if (this.constituency ) {
         res += this.constituency;
     }
@@ -24,10 +27,10 @@ baseboard.getColor = function(hex){
         case "Water" : return "aqua";
         case "Land" : return "brown";
     }
+
     if (hex.constituency) {
         let a = this.country[hex.constituency];
         if (a) {
-            console.log("a = ",a);
             return a.Color;
         }
         console.log("no value for" ,hex);
@@ -36,15 +39,10 @@ baseboard.getColor = function(hex){
     console.log("Black for" ,hex);
 
     return "black";
-    
 }
 
-baseboard.drawHex = function(ctx,hex,x,y,s){ 
-    ctx.fillStyle = this.getColor(hex);
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 1;
+baseboard.hexPath = function(ctx,x,y,s){
     ctx.beginPath();
-    //ctx.fillRect(x,y,s,s);
     ctx.moveTo(x+s*0.1,y);
     ctx.lineTo(x+s*0.9,y);
     ctx.lineTo(x+s*1.1,y+s/2); 
@@ -52,9 +50,53 @@ baseboard.drawHex = function(ctx,hex,x,y,s){
     ctx.lineTo(x+s*0.1,y+s);
     ctx.lineTo(x-s*0.1,y + s/2);
     ctx.lineTo(x+s*0.1,y);
-    ctx.fill();
-    ctx.stroke();
+}
 
+baseboard.hexEdge = function(ctx,x,y,s,angle){
+    switch(angle){
+    case 0:
+        ctx.moveTo(x+s*0.1,y);
+        ctx.lineTo(x+s*0.9,y);
+        return
+    case 1:
+        ctx.moveTo(x+s*0.9,y);
+        ctx.lineTo(x+s*1.1,y+s/2); 
+        return
+    case 2:
+        ctx.moveTo(x+s*1.1,y+s/2); 
+        ctx.lineTo(x+s*0.9,y+s);
+        return
+    case 3:
+        ctx.moveTo(x+s*0.9,y+s);
+        ctx.lineTo(x+s*0.1,y+s);
+        return
+    case 4:
+        ctx.moveTo(x+s*0.1,y+s);
+        ctx.lineTo(x-s*0.1,y + s/2);
+        return
+    case 5:
+        ctx.moveTo(x-s*0.1,y + s/2);
+        ctx.lineTo(x+s*0.1,y);
+        return
+    }
+}
+
+baseboard.strokeHex = function(ctx,hex,x,y,s){
+    switch (hex.hexType) {
+        case "Water": case "Land": 
+            return
+        default: 
+            this.hexPath(ctx,x,y,s);
+            ctx.lineWidth = 1;
+            ctx.strokeStyle = "black";
+            ctx.stroke();
+    }
+}
+
+baseboard.fillHex = function(ctx,hex,x,y,s){
+    this.hexPath(ctx,x,y,s);
+    ctx.fillStyle = this.getColor(hex);
+    ctx.fill();
 
     switch( hex.hexType){
         case "City": 
@@ -73,8 +115,8 @@ baseboard.drawHex = function(ctx,hex,x,y,s){
             ctx.arc(x + s/2,y+s/2,s/4,0,2*Math.PI);
             ctx.stroke();
     }
-    
 }
+
 
 baseboard.nToXy = function(n){
     return {
@@ -90,15 +132,58 @@ baseboard.xyToN = function(x,y){
     return x + y * this.w;
 }
 
+baseboard.setBorders = function() {
+    //special edges
+    for (p in this.map){
+        let mp = this.map[p];
+        if (! mp.inBoard()) continue
+
+        let adj = this.adjacent(p,true);
+        for (let a = 0; a < adj.length; a++) {
+            if (adj[a] === undefined) continue;
+            let ma = this.map[adj[a]];
+            if (! ma.inBoard()) continue
+
+            if (this.country[ma.constituency].Country === this.country[mp.constituency].Country ) continue
+            if (mp.border == undefined) mp.border = [];
+            mp.border.push(a);
+            
+        }
+
+    }
+}
 
 baseboard.draw = function(ctx,w,h){ 
     console.log("Drawing : ",w,h);
     s = Math.min(w/( this.w + 0.2), (h)/(this.h+0.5));
     console.log("S:" ,s, this.map.length)
+    //fill
     for (p in this.map) {
-        xy = this.nToXy(p); 
-        yp = xy.x %2 === 0 ? 0 : s/2;
-        this.drawHex(ctx,this.map[p],s*xy.x,yp + s*xy.y,s); 
+        let xy = this.nToXy(p); 
+        let yp = (xy.x %2) * s/2 ;
+        this.fillHex(ctx,this.map[p],s*xy.x,yp + s*xy.y,s); 
+    }
+    //normal edges
+    for (p in this.map) {
+        let xy = this.nToXy(p); 
+        let yp = (xy.x %2) * s/2 ;
+        this.strokeHex(ctx,this.map[p],s*xy.x,yp + s*xy.y,s); 
+    }
+    //special edges
+
+    for (p in this.map) {
+        mp = this.map[p];
+        if (mp.border === undefined) continue;
+        let xy = this.nToXy(p); 
+        let yp = (xy.x %2) * s/2 ;
+        ctx.strokeStyle="red";
+        ctx.lineWidth = 4;
+        ctx.beginPath();
+        for ( a in mp.border) {
+            this.hexEdge(ctx,s*xy.x,yp + s*xy.y,s,mp.border[a]);
+        }
+        ctx.stroke();
+        
     }
 }
 
@@ -121,6 +206,106 @@ baseboard.mposToXy = function(mx,my,w,h){
 }
 
 
+//returns an xy, or 'undefined' for steps off the board
+baseboard.stepXy = function(xy,angle) { 
+    let res = {x:xy.x,y:xy.y};
+    let up = res.x %2 ===0 ? 1 : 0;
+    switch(angle){
+    case 0:
+        res.y = res.y -1;
+        break;
+    case 1:
+        res.x = res.x + 1;
+        res.y = res.y - up;
+        break;
+    case 2:
+        res.x = res.x + 1;
+        res.y = res.y + 1 - up;
+        break;
+    case 3: 
+        res.y += 1;
+        break;
+    case 4:
+        res.x -= 1;
+        res.y += 1 - up;
+        break;
+    case 5:
+        res.x -= 1;
+        res.y -= up;
+    }
+    if (( res.x < 0) || (res.y <0) || (res.x >= this.w) ||(res.y >= this.h) ) return undefined;
+    return res
+
+}
+
+//returns an [] of ints for simple locations
+//allowU, allow Undefined in answer
+baseboard.adjacent = function(n,allowU){
+    let xy = this.nToXy(n)
+    res = [];
+    for (let i = 0; i < 6; i++ ){
+        let r = this.stepXy(xy,i);
+        if (r === undefined){
+            if (allowU ) res.push(undefined);
+            continue;
+        }
+        res.push(this.xyToN(r)); 
+    }
+    return res;
+}
+
+
+//Returns if a hex can connect to it's city, without the noUse hex
+baseboard.cityConnect = function(n,noUse){
+    let members = [n];
+    let i = 0;
+    while(true){
+        let mi = members[i];
+        let mpi = this.map[mi];
+        if (mpi.hexType === "City") return true;
+        let adj = this.adjacent(mi);
+        for (let a = 0; a < adj.length; a++) {
+            let ma = adj[a]
+            let mpa = this.map[ma]
+            if (ma === noUse) continue;
+            if (!mpa.inBoard()) continue
+            if (mpa.constituency !== mpi.constituency) continue;
+            if ( members.indexOf(ma) !== -1) continue;
+            console.log("ma",ma);
+            members.push(ma);
+        }
+        i ++;
+        if (i >= members.length) return false;
+    }
+}
+
+
+baseboard.tryGerrymander = function(n,constit){
+    let chex = this.map[n];
+    console.log("try gerry",n, constit);
+    if (chex.constituency == constit) return false;
+    console.log("tg - is change");
+    
+    if (chex.hexType != "Rural") return false;
+    console.log("tg - is rural");
+    adj = this.adjacent(n);
+    conFound = false;
+    for (let a in adj){
+        if (this.map[adj[a]].constituency == constit){
+            conFound = true;
+            break;
+        }
+    }
+    if (! conFound){
+        return false;
+    }
+    console.log("tg - is connected to new constit");
+    // todo check no break in constituency
+
+    this.map[n].constituency = constit;
+    return true;
+}
+
 //w and h cannot be zero
 function Board(w,h,country,map){
     res = Object.create(baseboard);
@@ -133,11 +318,13 @@ function Board(w,h,country,map){
     for (var i = 0; i < fsize; i++ ){
             res.map[i] = Object.create(basehex);
     }
+
     if (map !== undefined) {
         for (p in res.map ){
-            Object.assign(res.map[p],ob.map[p]);
+            Object.assign(res.map[p],map[p]);
         }
     }
+
 
     return res;
 }
--- play.js
+++ play.js
@@ -3,4 +3,27 @@
 //dep canvas.js
 //
 
-board =  
+board = Board(uk.w,uk.h,uk.country,uk.map);
+board.setBorders();
+can = readyCanvas(document.getElementById("cancan"));
+
+can.draw = function(){
+    console.log("Hello");
+    let ctx = can.ctx;
+    ctx.fillStyle = "#00ffcc";
+    ctx.fillRect(0,0,can.width,can.height);
+    board.draw(can.ctx,can.width,can.height);
+}
+
+
+can.onmousedown = can.mouser(function(e,x,y){
+    let bxy = board.mposToXy(x,y,cancan.width,cancan.height)
+    if (bxy === undefined) {
+        return;
+    }
+    let mapn = board.xyToN(bxy);
+    console.log(mapn, board.map[mapn]);
+});
+
+
+can.draw();
